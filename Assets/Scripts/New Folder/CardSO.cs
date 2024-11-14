6b8bab547062eab0ec47e52061c5d953cdc62164
Furydrack <marius.boulandet@gmail.com>
2024-11-14 16:35:25 +0100

    Card Scriptable Object

---
+++ Assets/Scripts/New Folder/CardSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card", menuName = "MiniVilles/Card")]
+public class CardSO : ScriptableObject
+{
+    public string name;
+    public Sprite sprite;
+}
