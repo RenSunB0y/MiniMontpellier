09d8bcd1a98a3f28926012784e9aaa8ebf5a447a
Furydrack <marius.boulandet@gmail.com>
2024-11-15 16:03:51 +0100

    ShopUI V1
    Implémentation quasi complète première version fonctionnelle

---
+++ Assets/Scripts/SO/CardSO.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card", menuName = "MiniVilles/Card")]
+public class CardSO : ScriptableObject
+{
+    public GameObject prefab;
+    public Sprite sprite;
+    public string name;
+    public string effect;
+    public string color;
+    public int cost;
+    public int gain;
+    public int[] dice;
+    public int amount;
+}
