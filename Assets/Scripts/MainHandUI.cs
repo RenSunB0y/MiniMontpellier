9149838d323f954675857be43dacba19e3f4265d
Furydrack <marius.boulandet@gmail.com>
2024-11-15 14:04:15 +0100

    Polish with DOTween

--- Assets/Scripts/MainHandUI.cs
+++ Assets/Scripts/MainHandUI.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine.EventSystems;
 using System.Data.Common;
+using System.Security.Cryptography;
+using System;
 
 public class HorizontalLayoutBehaviour : MonoBehaviour
 {
@@ -13,7 +16,13 @@ public class HorizontalLayoutBehaviour : MonoBehaviour
     [SerializeField]
     private List<CardSO> cards = new List<CardSO>();
     private int actualdCardId;
+    private float _cardPosY;
     private const float MIN_SPACE = 20;
+    private const int SHOW_HAND_Y = 0;
+    private const int HIDE_HAND_Y = -175;
+    private const float HAND_TRANS_DURATION = 0.3f;
+    private const float SELECTED_CARD_MOV_COEF = 300;
+    private const float SELECTED_CARD_DURATION = 0.15f;
     
     int count = 0;
     IEnumerator FillMain()
@@ -21,7 +30,7 @@ public class HorizontalLayoutBehaviour : MonoBehaviour
         yield return new WaitForSeconds(0.05f);
         cards.Add(cards[0]);
         UpdateMainHand();
-        if(count<7)
+        if(count<17)
         {
             count++;
             StartCoroutine("FillMain");
@@ -47,11 +56,23 @@ public class HorizontalLayoutBehaviour : MonoBehaviour
             c.GetComponent<Image>().sprite = card.sprite;
             c.GetComponent<OnPointerEnter>().MainHand = this;
             c.GetComponent<OnPointerEnter>().id = id;
+            _cardPosY = c.GetComponent<RectTransform>().position.y;
             id++;
         }
         transform.GetComponent<HorizontalLayoutGroup>().spacing = -(cards.Count * 6 + MIN_SPACE);
     }
 
+    public void ShowHand()
+    {
+        DOTween.To(()=> transform.GetComponent<HorizontalLayoutGroup>().padding, x=> transform.GetComponent<HorizontalLayoutGroup>().padding = x, new RectOffset(0,0,0,SHOW_HAND_Y), HAND_TRANS_DURATION);
+    }
+
+    public void HideHand()
+    {
+        DOTween.To(()=> transform.GetComponent<HorizontalLayoutGroup>().padding, x=> transform.GetComponent<HorizontalLayoutGroup>().padding = x, new RectOffset(0,0,0,HIDE_HAND_Y), HAND_TRANS_DURATION);
+    }
+
+    public float testtt;
     public void MouseOnCard(int id)
     {
         for(int i=0; i<transform.childCount; i++)
@@ -63,7 +84,10 @@ public class HorizontalLayoutBehaviour : MonoBehaviour
         selectedCardImage.color = new Color(1,1,1,0);
 
         SelectedCardZoom.sprite = selectedCardImage.sprite;
-        SelectedCardZoom.transform.position = new Vector2(selectedCardImage.transform.position.x-SelectedCardZoom.transform.lossyScale.x/2f, SelectedCardZoom.transform.position.y);
+        SelectedCardZoom.GetComponent<RectTransform>().position = selectedCardImage.GetComponent<RectTransform>().position;
+        SelectedCardZoom.rectTransform.localScale = selectedCardImage.rectTransform.localScale;
+        SelectedCardZoom.transform.DOMove(new Vector3(SelectedCardZoom.GetComponent<RectTransform>().position.x, _cardPosY + SELECTED_CARD_MOV_COEF*SelectedCardZoom.GetComponent<RectTransform>().lossyScale.y),SELECTED_CARD_DURATION);
+        SelectedCardZoom.transform.DOScale(SelectedCardZoom.rectTransform.localScale * testtt, SELECTED_CARD_DURATION);
         SelectedCardZoom.enabled = true;
     }
 
