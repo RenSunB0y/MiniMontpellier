f91392b4cb5638d8fb42d871da84150257286e6f
Furydrack <marius.boulandet@gmail.com>
2024-11-16 21:04:07 +0100

    Add amount visual and prefab canva

--- Assets/Scripts/CardTemplateConfig.cs
+++ Assets/Scripts/CardTemplateConfig.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Xml.Xsl;
 using NUnit.Framework.Internal;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,11 +12,13 @@ public class CardTemplateConfig : MonoBehaviour
     #region Prefab's children
     public Image art;
     public Image cardTemplate;
+    public List<Color> templatesColors;
     public Image icon;
     public Image monumentIcon;
     public TextMeshProUGUI gain;
     public TextMeshProUGUI cost;
     public TextMeshProUGUI amount;
+    public Transform amountVisual;
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI effect;
     public TextMeshProUGUI dice;
@@ -23,12 +27,22 @@ public class CardTemplateConfig : MonoBehaviour
     public List<Sprite> templates;
     public List<Sprite> icons;
     private List<string> templatesChoices = new List<string>{"Bleu","Vert","Rouge","Violette","Jaune","Grise"};
+    private Dictionary<string,Color> colorChoice = new Dictionary<string, Color>();
     public CardSO card;
     public bool isInShop;
 
     public const int test = 1;
     public void Awake()
     {
+        colorChoice = new Dictionary<string, Color>()
+        {
+            {"Bleu",templatesColors[0]},
+            {"Vert",templatesColors[1]},
+            {"Rouge",templatesColors[2]},
+            {"Violette",templatesColors[3]},
+            {"Jaune",templatesColors[4]},
+            {"Grise",templatesColors[5]},
+        };
         if(card!=null)
             Load(card,isInShop);
     }
@@ -41,6 +55,9 @@ public class CardTemplateConfig : MonoBehaviour
         string iconName = data.type == "Monument" ? "Spécial" : data.type;
         icon.sprite = data.mainIcon;
         cardTemplate.sprite = templates[templatesChoices.IndexOf(data.color)]; // Template
+
+        int t = Random.Range(1,7);
+
         if(inShop)
         {
             if(data.amount==0)
@@ -52,6 +69,15 @@ public class CardTemplateConfig : MonoBehaviour
 
             amount.text = data.amount.ToString();
             cost.text = data.cost.ToString();
+        }
+        else
+        {
+            for(int i=0; i<amountVisual.childCount; i++)
+            {
+                amountVisual.GetChild(i).GetComponent<Image>().color = templatesColors[templatesColors.IndexOf(colorChoice[data.color])];
+                amountVisual.GetChild(i).gameObject.SetActive(i<t && t>=2);
+            }
+            amountVisual.GetComponent<Image>().enabled = t>=2;
         }
         amount.gameObject.SetActive(inShop);
         cost.transform.parent.gameObject.SetActive(inShop);
