12825aca8d5b08c6adc54ed2377e9d068a0cb45d
Samuel Bugeas <samuel.bugeas@gmail.com>
2024-11-17 16:40:39 +0100

    Creation SoundManager + début config

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        //---- Singleton
+        if(instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        //---------------
+
+
+    }
+
+    //------- MUSIC
+    public UnityEvent onMainMenuPlay = new UnityEvent();
+    public UnityEvent onMainMenuExit = new UnityEvent();
+
+    public UnityEvent onGameMusicPlay = new UnityEvent();
+    public UnityEvent onGameMusicExit = new UnityEvent();
+
+    //------- SOUND EFFECT
+
+    //Coin
+    public UnityEvent onCoinSound_1 = new UnityEvent();
+    public UnityEvent onCoinSound_2 = new UnityEvent();
+
+    //Dice
+    public UnityEvent onDicePick_1 = new UnityEvent();
+    public UnityEvent onDiceRoll_1 = new UnityEvent();
+    public UnityEvent onDiceRoll_2 = new UnityEvent();
+    public UnityEvent onDiceShake = new UnityEvent();
+
+    //Gain/Lose
+    public UnityEvent onGain = new UnityEvent();
+    public UnityEvent onLose = new UnityEvent();
+
+    //Cards
+
+    //Button
+    public UnityEvent onButtonSound = new UnityEvent();
+}
