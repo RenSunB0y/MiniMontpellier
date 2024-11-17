8bb90729f197546cc705d814e63e87542be3de8e
RenSunB0y <150889605+RenSunB0y@users.noreply.github.com>
2024-11-17 14:34:47 +0100

    update

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -10,6 +10,6 @@ public class ChangeScene : MonoBehaviour
     }
     public void StartGame()
     {
-        SceneManager.LoadScene("YvannDevScene");
+        SceneManager.LoadScene("Game Scene");
     }
 }
